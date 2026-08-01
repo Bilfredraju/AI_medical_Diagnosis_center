ae55bbe55329d9a63a054e5c410f84df46724fbd
bilfred <bilfredraju123@gmail.com>
2026-08-01 10:37:17 +0530

    Integrate MLflow experiment tracking for TensorFlow training

--- src/tensorflow_model/train.py
+++ src/tensorflow_model/train.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.keras.callbacks import TensorBoard
+import mlflow
+import mlflow.tensorflow
+
+
+
 
 from src.tensorflow_model.model import build_model
 
@@ -52,6 +57,8 @@ test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 # Build Model
 model = build_model()
 
+mlflow.set_experiment("Brain Tumor Diagnosis - TensorFlow CNN")
+
 # Compile Model
 model.compile(
     optimizer="adam",
@@ -65,6 +72,51 @@ tensorboard_callback = TensorBoard(
     log_dir="logs",
     histogram_freq=0
 )
+
+with mlflow.start_run():
+
+    mlflow.log_param("epochs", EPOCHS)
+    mlflow.log_param("batch_size", BATCH_SIZE)
+    mlflow.log_param("image_size", IMAGE_SIZE)
+    mlflow.log_param("optimizer", "adam")
+
+    history = model.fit(
+        train_dataset,
+        validation_data=validation_dataset,
+        epochs=EPOCHS,
+        callbacks=[tensorboard_callback]
+    )
+
+    mlflow.log_metric(
+        "train_accuracy",
+        history.history["accuracy"][-1]
+    )
+
+    mlflow.log_metric(
+        "validation_accuracy",
+        history.history["val_accuracy"][-1]
+    )
+
+    mlflow.log_metric(
+        "train_loss",
+        history.history["loss"][-1]
+    )
+
+    mlflow.log_metric(
+        "validation_loss",
+        history.history["val_loss"][-1]
+    )
+
+    mlflow.tensorflow.log_model(
+        model,
+        name="brain_tumor_tensorflow_model"
+    )
+
+
+
+
+
+
 
 history = model.fit(
     train_dataset,
